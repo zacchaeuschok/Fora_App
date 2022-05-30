7789e37561abcb9cfde81f09bcd8d1158c56a4f9
Your-Full-Name <your-email-address>
2022-05-30 15:32:27 +0800

    Set up .env file

---
+++ Fora/env.d.ts
@@ -0,0 +1,4 @@
+declare module '@env' {
+  export const REACT_NATIVE_SUPABASE_URL: string;
+  export const REACT_NATIVE_SUPABASE_ANON_KEY: string;
+}
--- Fora/lib/supabase.ts
+++ Fora/lib/supabase.ts
@@ -1,8 +1,11 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js'
+import { REACT_NATIVE_SUPABASE_URL, REACT_NATIVE_SUPABASE_ANON_KEY} from "@env"
 
-const supabaseUrl = YOUR_REACT_NATIVE_SUPABASE_URL
-const supabaseAnonKey = YOUR_REACT_NATIVE_SUPABASE_ANON_KEY
+
+const supabaseUrl = REACT_NATIVE_SUPABASE_URL || ''
+const supabaseAnonKey = REACT_NATIVE_SUPABASE_ANON_KEY || ''
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   localStorage: AsyncStorage as any,
