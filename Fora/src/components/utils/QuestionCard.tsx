484b318458d23442ecdae02d9d053dd0a04b2df6
zacchaeuschok <zacchaeuschok2014@gmail.com>
2022-06-05 22:50:11 +0800

    added question cards

---
+++ Fora/src/components/utils/QuestionCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { View, Image, Text } from "react-native";
+import { Section, SectionContent, SectionImage } from "react-native-rapi-ui";
+import { COLORS, SIZES, SHADOWS, assets } from "../../../constants";
+
+const QuestionCard = ({ data }) => {
+    const navigation = useNavigation();
+
+    return (
+        <Section>
+            <SectionImage
+                source={data.image}
+                resizeMode="cover"
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    borderTopLeftRadius: SIZES.font,
+                    borderTopRightRadius: SIZES.font,
+                }} />
+            <SectionContent>
+                <Text>This is a Section</Text>
+            </SectionContent>
+        </Section>
+    );
+};
+
+export default QuestionCard;
--- Fora/src/initSupabase.ts
+++ Fora/src/initSupabase.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 import { REACT_NATIVE_SUPABASE_URL, REACT_NATIVE_SUPABASE_ANON_KEY} from "@env"
 
 
-const supabaseUrl = REACT_NATIVE_SUPABASE_URL || ''
-const supabaseAnonKey = REACT_NATIVE_SUPABASE_ANON_KEY || ''
+const supabaseUrl = 'https://zckfzigvesrqdzbqgxju.supabase.co' || ''
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpja2Z6aWd2ZXNycWR6YnFneGp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTI1MDU2MjgsImV4cCI6MTk2ODA4MTYyOH0.fNYJ8N9fhM7AphyrTc4B9pfpAqgmL8pU-G8ml3mXuMY' || ''
 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
