f771f6a04c08495de7548813ee013b53d958919e
zacchaeuschok <zacchaeuschok2014@gmail.com>
2022-07-23 14:58:31 +0800

    fixed avatar aspect ratio

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -34,7 +34,7 @@ export default function Avatar({ url, size, onUpload }) {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [3, 3],
       quality: 1,
     });
 
