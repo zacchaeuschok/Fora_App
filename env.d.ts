b420db8aa63c34829e4ed3cba3ebbc8b5a0cfeb9
Tan Xuan Yun <your-email-address>
2022-07-24 13:33:23 +0800

    env file

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,4 @@
 declare module '@env' {
-    export const REACT_APP_SUPABASE_URL: string;
-    export const REACT_APP_SUPABASE_ANON_KEY: string;
+    export const REACT_NATIVE_SUPABASE_URL: string;
+    export const REACT_NATIVE_SUPABASE_ANON_KEY: string;
 }
